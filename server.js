baf656e5de6009b61576e3e281d9b06c91b40eb2
ruuuh20 <pkim2320@gmail.com>
2019-01-03 15:12:26 -0500

    fix comma

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const handleApi = (req, res) => {
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
